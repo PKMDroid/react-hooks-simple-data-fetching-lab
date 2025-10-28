fc1edd95c12622e4a35b5e5d35cb95f53c0c85e9
PKMDroid <princekibali222@gmail.com>
2025-10-28 23:39:44 +0300

    updates App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,37 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+const DOG_API_URL = 'https://dog.ceo/api/breeds/image/random';
+
+function App() {
+  const [dogImageUrl, setDogImageUrl] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(DOG_API_URL)
+      .then(response => response.json())
+      .then(data => {
+        setDogImageUrl(data.message);
+      })
+      .catch(error => {
+        console.error("Failed to fetch dog image:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        dogImageUrl && (
+          <img src={dogImageUrl}alt="A Random Dog"/>
+        )
+      )}
+    </div>
+  );
+}
+
+export default App;
